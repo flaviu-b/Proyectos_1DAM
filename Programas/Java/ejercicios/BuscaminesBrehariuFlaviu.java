d6045cb4f3a7f51ca3f17669375b691a306c8305
Flaviu Mihai Brehariu <flaviumihaib@gmail.com>
2024-02-20 13:08:18 +0100

    todo hecho en clase de prg

--- Programas/Java/ejercicios/BuscaminesBrehariuFlaviu.java
+++ Programas/Java/ejercicios/BuscaminesBrehariuFlaviu.java
@@ -3,8 +3,13 @@ import java.util.Random;
 
 import funciones.Teclat;
 public class BuscaminesBrehariuFlaviu {
-    static char tapado = ' ';
+    static char tapado = 'X';
+    static char destapado =' ';
+    static char bandera = 'P';
+    static char bomba = '#';
+
     public static void main(String[] args) {
+        boolean mostrarMinas;
         char tableroVisible[][] = creaTableroVisible();
         int minasMax = (tableroVisible.length * tableroVisible[0].length);
         int Qminas = Teclat.lligInt("Cuantas minas quieres que tenga el tablero?(3-"+minasMax+"): ", 3, minasMax);
@@ -15,9 +20,9 @@ public class BuscaminesBrehariuFlaviu {
         do{
             posicionF = Teclat.lligInt("Dame la posición de la casilla con la que desea interactuar.\nFila",1,tableroVisible[0].length) - 1;
             posicionC = Teclat.lligInt("Columna",1,tableroVisible.length) - 1;
-        }while(posicionF > 0 && posicionC > 0 && posicionF < tableroVisible.length && posicionC < tableroVisible.length);        
-        int qma = qma(tableroMinas, posicionF, posicionC);
-        System.out.println(qma);
+        }while(posicionF < 0 && posicionC < 0 && posicionF > tableroVisible.length && posicionC > tableroVisible[0].length);        
+            int qma = qma(tableroMinas, posicionF, posicionC);
+
     }
 
     private static char[][] creaTableroVisible(){
@@ -52,7 +57,7 @@ public class BuscaminesBrehariuFlaviu {
                 i--;
             }
         }
-        // Esta parte solo muestra la matriz tableroMinas
+        /*Esta parte solo muestra la matriz tableroMinas
         for (int i = 0; i < tablero.length; i++) {
             for (int j = 0; j < tablero[i].length; j++) {
             //    System.out.print("[" + tablero[i][j] + "]");
@@ -61,6 +66,7 @@ public class BuscaminesBrehariuFlaviu {
             }
             System.out.println();
         }
+         */ 
         return tablero;
     }
 
@@ -94,7 +100,53 @@ public class BuscaminesBrehariuFlaviu {
         
         return contador;
     }
-    
+    private static boolean destapada(char tableroVisible[][], int posicionF, int posicionC){
+
+        if (tableroVisible[posicionF][posicionC] != tapado) return false;
+        else return true; 
+    }
+    private static int qdestapadas(char tableroVisible[][]){
+        int contador = 0;
+        for (int i = 0; i < tableroVisible.length; i++) {
+            for (int j = 0; j < tableroVisible[i].length; j++) {
+                if (destapada(tableroVisible,i,j) == true) contador ++; 
+            }
+        }
+        return contador;
+    }
+    private static void mostrarTablero(char tableroVisible[][], boolean tableroMinas[][], boolean mostrarMinas){
+        if (mostrarMinas) {
+            for (int i = 0; i < tableroMinas.length; i++) {
+                for (int j = 0; j < tableroMinas[i].length; j++) {
+                    if (tableroMinas[i][j]) tableroVisible[i][j] = bomba;
+                    System.out.print("[" + tableroVisible[i][j] + "]"); 
+                }
+                System.out.println();
+            }
+        }else{
+            for (int i = 0; i < tableroMinas.length; i++) {
+                for (int j = 0; j < tableroMinas[0].length; j++) {
+                    System.out.print("[" + tableroVisible[i][j] + "]");                     
+                }
+                System.out.println();
+            }
+
+        }
+
+
+    }
+
+    private static boolean minada(boolean tableroMinas[][], int posicionF, int posicionC){
+        return tableroMinas[posicionF][posicionC];        
+    }
+    private static boolean pica(char tableroVisible[][], boolean tableroMinas[][], int posicionF, int posicionC){
+        if (! minada(tableroMinas, posicionF, posicionC)){
+            destapa();
+            return false;
+        }else{
+            return true;
+        }
+    }
 
 
 
